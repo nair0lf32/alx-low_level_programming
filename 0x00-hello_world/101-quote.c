51b0a9213f42066bb5f1eba38fbcb01d9ebe6654
nair0lf32 <florianedem@gmail.com>
2022-03-10 16:04:32 +0100

    fixed task 7

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
-*main -  prints to stderr
-*Return: 0
+*main -  writes to stderr
+*Return: 1
 */
 int main(void)
 {
-perror("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
-return (0);
+write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+return (1);
 }
