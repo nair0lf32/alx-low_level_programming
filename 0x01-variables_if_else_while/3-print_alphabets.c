084c0f69fd3beed1e76171ac1eecb55dfa06ee1b
nair0lf32 <florianedem@gmail.com>
2022-03-11 15:51:34 +0100

    attempt task 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+*main -  print the alphabet (lowercase)
+*Return: 0
+*/
+int main(void)
+{
+int i;
+for (i = 'a'; i <= 'z'; i++)
+{
+putchar(i);
+}
+for (i = 'A'; i <= 'Z'; i++)
+{
+putchar(i);
+}
+putchar('\n');
+return (0);
+}
