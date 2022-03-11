947e6e75a3d3e85c8de70495e70c079a310ae709
nair0lf32 <florianedem@gmail.com>
2022-03-11 16:07:45 +0100

    do task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+*main -  print numbers from 0 to 9
+*Return: 0
+*/
+int main(void)
+{
+int i;
+for (i = 0; i <= 9; i++)
+{
+putchar(i);
+}
+putchar('\n');
+return (0);
+}
