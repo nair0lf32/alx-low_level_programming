20df35074b5e51b8f54c20b3628d6e395a686ff1
nair0lf32 <florianedem@gmail.com>
2022-03-11 18:00:19 +0100

    attempt task 12

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+*main -  print comibnation of three-digit numbers
+*Return: 0
+*/
+int main(void)
+{
+int i, j;
+for (i = 0; i < 98; i++)
+{
+for (j = i + 1; j < 99; j++)
+{
+putchar(i / 10 + '0');
+putchar(i % 10 + '0');
+putchar(' ');
+putchar(j / 10 + '0');
+putchar(j % 10 + '0');
+if (i / 10 == 8 && j % 10 == 9)
+{
+break;
+}
+else
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+putchar('\n');
+return (0);
+}
