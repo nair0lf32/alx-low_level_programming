cf662b60067a876c9b6680dc58d7d64c47e9d6e5
nair0lf32 <florianedem@gmail.com>
2022-03-11 15:29:53 +0100

    do task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+*main -  print the alphabet (lowercase)
+*Return: 0
+*/
+int main(void)
+{
+for (int i = 'a'; i <= 'z'; i++)
+{
+putchar(i);
+}
+return (0);
+}
