3ac351bfab2e39a8bace1608b2a77d6f10a94369
nair0lf32 <florianedem@gmail.com>
2022-03-11 17:37:41 +0100

    fix #5 for task 10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -13,13 +13,13 @@ for (j = i + 1; j < 10; j++)
 {
 putchar(i % 10 + '0');
 putchar(j % 10 + '0');
+putchar(',');
+putchar(' ');
 if (i == '8' && j == '9')
 {
 continue;
 }
 }
-putchar(',');
-putchar(' ');
 }
 putchar('\n');
 return (0);
