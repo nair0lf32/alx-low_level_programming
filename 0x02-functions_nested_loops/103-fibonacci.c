52f79abb28c999fcbd3d8f2a6e9cd359be309964
nair0lf32 <florianedem@gmail.com>
2022-03-16 00:42:28 +0100

    do task 15

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+*main - prints fibonacci numbers (sum of even-valued terms)
+*Return: nothing (void)
+*/
+int main(void)
+{
+unsigned long i, j, k, count, sum;
+i = sum = 0;
+j = 1;
+for (count = 0; count < 50; count++)
+{
+k = i + j;
+i = j;
+j = k;
+if (k % 2 == 0 && k < 4000000)
+{
+{
+sum += k;
+}
+}
+printf("%lu", sum);
+return (0);
+}
