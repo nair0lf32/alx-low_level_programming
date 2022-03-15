38d4413e99aa72521fdaa89a76bf15fbd887683b
nair0lf32 <florianedem@gmail.com>
2022-03-15 22:25:26 +0100

    do task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+*_abs -  print the absolute value of a number
+*@n: input number
+*Return: 0
+*/
+int _abs(int n)
+{
+if (n >= 0)
+{
+_putchar(n);
+}
+else
+{
+_putchar(-n);
+}
+return (0);
+}
