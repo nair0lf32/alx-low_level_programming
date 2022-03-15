a04d48628d20323e1d6ac6fc27a2e4cc16485d29
nair0lf32 <florianedem@gmail.com>
2022-03-15 23:25:15 +0100

    re-fix task 11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -7,33 +7,16 @@
 */
 void print_to_98(int n)
 {
-if (n < 98)
+if (n >= 98)
 {
-while (n <= 98)
-{
-printf("%d", n);
-if (n != 98)
-{
-_putchar(',');
-}
-n++;
-}
-}
-else if (n > 98)
-{
-while (n >= 98)
-{
-printf("%d", n);
-if (n != 98)
-{
-_putchar(',');
-}
-n--;
-}
+while (n > 98)
+printf("%d, ", n--);
+printf("%d\n", n);
 }
 else
 {
-printf("%d", 98);
+while (n < 98)
+printf("%d, ", n++);
+printf("%d\n", n);
 }
-_putchar('\n');
 }
