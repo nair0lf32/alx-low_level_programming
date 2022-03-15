681440dd9734de39b3d75aa796ad393842262eac
nair0lf32 <florianedem@gmail.com>
2022-03-15 19:50:06 +0100

    do task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+*print_alphabet -  print the alphabet
+*Return: void (nothing)
+*/
+void print_alphabet(void)
+{
+int c;
+for (c = 'a'; c <= 'z'; c++)
+{
+_putchar (c);
+}
+_putchar (10);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -4,3 +4,4 @@
 */
 
 int _putchar(char);
+void print_alphabet(void);
