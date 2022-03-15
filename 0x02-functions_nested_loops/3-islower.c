1f6ff14803ceca0c401be2e961ab7a6b16805619
nair0lf32 <florianedem@gmail.com>
2022-03-15 20:38:57 +0100

    fix doc for betty

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
-*print_alphabet_x10 -  check if a letter is lowercase
+*_islower -  check if a letter is lowercase
 *Return: 1 if lowercase, 0 if not
 */
 int _islower(int c)
