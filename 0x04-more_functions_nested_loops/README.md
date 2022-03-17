0063346becd31124426b1841cfc8bce9b959226e
nair0lf32 <florianedem@gmail.com>
2022-03-17 12:37:36 +0100

    do task 0

---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,3 @@
+# More functions, nested loops
+
+but wait...there's more!
---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+/**
+*File:main.h
+*Desc:header file with function prototypes
+*/
+char _putchar(char c);
+int _isupper(int c);
+
+#endif /* MAIN_H */
