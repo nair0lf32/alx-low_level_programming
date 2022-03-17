6d89c2a77830337fac9841f282bfce71a36d941a
nair0lf32 <florianedem@gmail.com>
2022-03-17 12:57:00 +0100

    do task 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+*print_numbers -  print numbers (lol 0-9)
+*Return: nothing
+*/
+void print_numbers(void)
+{
+int n;
+for (n = 0; n <= 9; n++)
+{
+_putchar(n + '0');
+}
+_putchar('\n');
+}
