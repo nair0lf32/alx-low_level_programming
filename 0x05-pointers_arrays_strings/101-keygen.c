aa0e3bfeebf42d4a9bf210bdd7a3950990bf8b24
nair0lf32 <florianedem@gmail.com>
2022-03-21 21:37:32 +0100

    do task 11

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+* main - generates a random password and print it
+*
+* Return: 0
+*/
+int main(void)
+{
+char c;
+int i;
+srand(time(0));
+while (i <= 2645)
+{
+c = rand() % 128;
+i += c;
+putchar(c);
+}
+putchar(2772 - i);
+return (0);
+}
