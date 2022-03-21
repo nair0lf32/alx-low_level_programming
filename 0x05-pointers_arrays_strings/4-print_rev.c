8a9c273661d722c226c0d4f0732fea1bfaaf885d
nair0lf32 <florianedem@gmail.com>
2022-03-21 14:26:17 +0100

    fix task 4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -16,3 +16,18 @@ len--;
 }
 _putchar('\n');
 }
+/**
+*_strlen -  determine the length of a string
+*@s: a pointer to a string
+*Return: the length of the string
+*/
+int _strlen(char *s)
+{
+int i = 0;
+
+while (s[i] != '\0')
+{
+i++;
+}
+return (i);
+}
