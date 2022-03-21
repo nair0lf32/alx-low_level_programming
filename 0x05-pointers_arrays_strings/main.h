273c212aa5c7fa68dc15981e8d6d16771d4e1afe
nair0lf32 <florianedem@gmail.com>
2022-03-21 14:16:47 +0100

    do task 3

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,12 +1,13 @@
 #ifndef MAIN_H
 #define MAIN_H
 /**
- *File:main.h
- *Desc:header file with function prototypes
- */
+*File:main.h
+*Desc:header file with function prototypes
+*/
 char _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
+void _puts(char *str);
 
 #endif /* MAIN_H */
