5f16a49d45c12a36d0a87b10c491ac699d0c9d99
nair0lf32 <florianedem@gmail.com>
2022-03-21 21:19:06 +0100

    fix task 10

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -8,23 +8,16 @@ int _atoi(char *s)
 {
 int i = 1;
 unsigned int num = 0;
-do {
+do{
 if (*s == '-')
 {
-i *= -1;
+i = -1;
 }
-else if (*s >= '0' && *s <= '9')
+if (*s >= '0' && *s <= '9')
 {
 num = num * 10 + (*s - '0');
 }
-else if (num > 0)
-{
-break;
-}
-else
-{
-return (0);
-}
-} while (*s++);
-return (num *i);
+s++;
+} while (*s != '\0');
+return (num * i);
 }
