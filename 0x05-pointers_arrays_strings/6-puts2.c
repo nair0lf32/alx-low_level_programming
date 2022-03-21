fbe85b3746e4a5cae69445616e4fdf8b61c49c1b
nair0lf32 <florianedem@gmail.com>
2022-03-21 15:07:53 +0100

    do task 6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+*puts2 -  prints every other char of a string
+*starting with the first char
+*@str: a pointer to a string
+*Return: nothing
+*/
+void puts2(char *str)
+{
+int i = 0;
+while (str[i] != '\0')
+{
+if (i % 2 == 0)
+{
+_putchar(str[i]);
+i++;
+}
+}
+_putchar('\n');
+}
