9e2297573f227fb99cb67fa86ee750d9a64936cc
nair0lf32 <florianedem@gmail.com>
2022-03-23 12:40:50 +0100

    do task 0

---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+/**
+*File:main.h
+*Desc:header file with function prototypes
+*/
+char _putchar(char c);
+char *_strcat(char *dest, char *src);
+
+#endif /* MAIN_H */
