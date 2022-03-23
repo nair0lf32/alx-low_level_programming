aa2f956dc70dd7a03c059cb1e80b291c5d79a709
nair0lf32 <florianedem@gmail.com>
2022-03-23 21:49:59 +0100

    change solution for task 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -6,25 +6,21 @@
 */
 char *cap_string(char *str)
 {
-int i = 0;
-while (str[++i])
+int i, j;
+char separators[] = " \t\n,;.!?\"(){}";
+for (i = 0; str[i] != '\0'; i++)
 {
-while (!(str[i] >= 'a' && str[i] <= 'z'))
-i++;
-if (str[i - 1] == ' ' ||
-str[i - 1] == '\t' ||
-str[i - 1] == '\n' ||
-str[i - 1] == ',' ||
-str[i - 1] == ';' ||
-str[i - 1] == '.' ||
-str[i - 1] == '!' ||
-str[i - 1] == '?' ||
-str[i - 1] == '"' ||
-str[i - 1] == '(' ||
-str[i - 1] == ')' ||
-str[i - 1] == '{' ||
-str[i - 1] == '}')
-str[i] -= 32;
+if (str[0] >= 97 && str[0] <= 122)
+{
+str[0] = str[0] - 32;
 }
-return (str);
+for (j = 0; separators[j] != '\0'; j++)
+{
+if (str[i] == separators[j] && str[i + 1] >= 97 && str[i + 1] <= 122)
+{
+str[i + 1] = str[i + 1] - 32;
+}
+}
+}
+return (_strcat);
 }
