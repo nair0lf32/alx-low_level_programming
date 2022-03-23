d7bcd1af7b2d662637498cb7475c0b491b30cfdc
nair0lf32 <florianedem@gmail.com>
2022-03-23 21:43:14 +0100

    fix task 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -2,17 +2,29 @@
 /**
 *cap_string - capitalizes a string characters
 *@str: pointer to the string
-*Return: pointer to the string 
+*Return: pointer to the string
 */
 char *cap_string(char *str)
 {
-int i;
-for (i = 0; str[i] != '\0'; i++)
+int i = 0;
+while (str[++i])
 {
-if (str[i] >= 'a' && str[i] <= 'z')
-{
-str[i] = str[i] + 32;
-}
+while (!(str[i] >= 'a' && str[i] <= 'z'))
+i++;
+if (str[i - 1] == ' ' ||
+str[i - 1] == '\t' ||
+str[i - 1] == '\n' ||
+str[i - 1] == ',' ||
+str[i - 1] == ';' ||
+str[i - 1] == '.' ||
+str[i - 1] == '!' ||
+str[i - 1] == '?' ||
+str[i - 1] == '"' ||
+str[i - 1] == '(' ||
+str[i - 1] == ')' ||
+str[i - 1] == '{' ||
+str[i - 1] == '}')
+str[i] -= 32;
 }
 return (str);
 }
