2cf7c391ddd6b25c1fa65527c9e8d77318ef9a6c
nair0lf32 <florianedem@gmail.com>
2022-03-23 22:30:13 +0100

    fix 74sk 7

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -6,14 +6,13 @@
 */
 char *leet(char *str)
 {
-int i,j;
+int i, j;
 char characters[] = "ol_ea__t";
 for (i = 0; str[i] != '\0'; i++)
 {
 for (j = 0; characters[j] != '\0'; j++)
 {
-if (str[i] == characters[j] ||
-str[i] == (characters[j] - 32))
+if (str[i] == characters[j] || str[i] == (characters[j] - 32))
 {
 str[i] = j + '0';
 }
