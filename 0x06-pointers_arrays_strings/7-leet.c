26448813b37800364a9f345706ece29de7666fac
nair0lf32 <florianedem@gmail.com>
2022-03-23 13:25:34 +0100

    d0 745k 7

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+*leet - encode a string to 1337
+*@str: pointer to the string
+*Return: pointer to the string 
+*/
+char *leet(char *str)
+{
+int i, j;
+char *leet[] = {
+"aAeEoOtTlL",
+"4433007711",
+"@43071",
+"szZ",
+"1337"
+};
+for (i = 0; str[i] != '\0'; i++)
+{
+for (j = 0; leet[0][j] != '\0'; j++)
+{
+if (str[i] == leet[0][j] || str[i] == leet[0][j] - 32)
+{
+str[i] = leet[2][j];
+}
+}
+}
+return (str);
+}
