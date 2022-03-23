cc2a983e1d961683b58f5aae54ed2d68c755595b
nair0lf32 <florianedem@gmail.com>
2022-03-23 22:21:27 +0100

    ch4nge s01u7i0n f0r 74sk 7

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -6,14 +6,13 @@
 */
 char *leet(char *str)
 {
-int i=0, j;
-char characters[8] = {'O', 'L', '_', 'E', 'A', '_', '_', 'T'};
-while (str[++i])
+int i,j;
+char characters[] = "ol_ea__t";
+for (i = 0; str[i] != '\0'; i++)
 {
-for (j = 0; j <= 7; j++)
+for (j = 0; characters[j] != '\0'; j++)
 {
-if ( str[i] == characters[j] ||
-str[i] - 32 == characters[j]) 
+if (str[i] == characters[j] || str[i] == (characters[j] - 32))
 {
 str[i] = j + '0';
 }
