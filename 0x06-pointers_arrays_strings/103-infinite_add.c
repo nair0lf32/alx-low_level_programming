a9641f8895e80080e8fa4235d670016259a1a55b
nair0lf32 <florianedem@gmail.com>
2022-03-24 09:36:39 +0100

    betty being annoying again

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -12,59 +12,37 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 int limit = 0, i = 0, j = 0, nums = 0;
 int num1 = 0, num2 = 0, temp_r = 0;
 while (*(n1 + i) != '\0')
-{
-i++;
-}
+{ i++; }
 while (*(n2 + j) != '\0')
-{
-j++;
-}
+{ j++; }
 i--;
 j--;
 if (j >= size_r || i >= size_r)
-{
-return (0);
-}
+{ return (0); }
 while (j >= 0 || i >= 0 || limit == 1)
 {
 if (i < 0)
-{
-num1 = 0;
-}
+{ num1 = 0; }
 else
-{
-num1 = *(n1 + i) - '0';
-}
+{ num1 = *(n1 + i) -'0'; }
 if (j < 0)
-{
-num2 = 0;
-}
+{ num2 = 0; }
 else
-{
-num2 = *(n2 + j) - '0';
-}
+{ num2 = *(n2 + j) -'0'; }
 temp_r = num1 + num2 + limit;
 if (temp_r >= 10)
-{
-limit = 1;
-}
+{ limit = 1; }
 else
-{
-limit = 0;
-}
+{ limit = 0; }
 if (nums >= (size_r - 1))
-{
-return (0);
-}
+{ return (0); }
 *(r + nums) = (temp_r % 10) + '0';
 nums++;
 j--;
 i--;
 }
 if (nums == size_r)
-{
-return (0);
-}
+{ return (0); }
 *(r + nums) = '\0';
 rev_string(r);
 return (r);
