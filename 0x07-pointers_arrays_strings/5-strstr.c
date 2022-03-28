8a4859a0a156499a82ba90913c949550b5d594b3
nair0lf32 <florianedem@gmail.com>
2022-03-28 15:28:29 +0100

    do task 5

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+*_strpbrk - locate a substring
+*@haystack: string to be searched (nice one)
+*@needle: substring to be searched for
+*Return: the beginning of the substring in the string or NULL if not found
+*/
+char *_strstr(char *haystack, char *needle)
+{
+int i, j, k;
+for (i = 0; haystack[i] != '\0'; i++)
+{
+for (j = 0; needle[j] != '\0'; j++)
+{
+if (haystack[i + j] != needle[j])
+{
+break;
+}
+}
+if (needle[j] == '\0')
+{
+return (haystack + i);
+}
+}
+return (0);
+}
