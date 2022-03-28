15f830cb46c452f55779c7dbeb627e40ad7ea88b
nair0lf32 <florianedem@gmail.com>
2022-03-28 16:07:50 +0100

    fix style for betty

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -1,7 +1,7 @@
 #include "main.h"
 #include <stdio.h>
 /**
-*print_diagsums- prints  the sum of the two diagonals 
+*print_diagsums- prints  the sum of the two diagonals
 *of a square matrix of integers.
 *@a: the said matrix
 *@size: the size of the matrix
