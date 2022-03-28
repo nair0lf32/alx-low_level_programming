948f2c9d6887af55fec4d13384a7e958250f2d33
nair0lf32 <florianedem@gmail.com>
2022-03-28 13:17:14 +0100

    do task 0

---
+++ 0x07-pointers_arrays_strings/README.md
@@ -0,0 +1,3 @@
+# Even More Pointers, Arrays, and Strings
+
+The xhibit (yo dawg) meme really sums it all.
---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+/**
+ *File:main.h
+ *Desc:header file with function prototypes
+ */
+char _putchar(char c);
+char *_memset(char *s, char b, unsigned int n);
+
+#endif /* MAIN_H */
