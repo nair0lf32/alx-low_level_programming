c7fcbfb150273712d7bed0355e7aa0db8f1e972b
nair0lf32 <florianedem@gmail.com>
2022-03-30 13:07:29 +0100

    fix task 6

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -10,13 +10,17 @@ if (n <= 1)
 {
 return (0);
 }
-if (n == 2)
+else if (n == 2)
 {
 return (1);
 }
-if (n % 2 == 0)
+else if (n % 2 == 0)
 {
 return (0);
 }
+else
+{
 return (is_prime_number(n - 1));
 }
+}
+
