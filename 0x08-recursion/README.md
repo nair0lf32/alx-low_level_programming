cdcc5df5441680b9ae34be739c9e692946bb8d64
nair0lf32 <florianedem@gmail.com>
2022-03-30 12:12:06 +0100

    do task 0

---
+++ 0x08-recursion/README.md
@@ -0,0 +1,3 @@
+# Recursion
+
+A function that call itself ? That's looping with extra-steps
---
+++ 0x08-recursion/main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+/**
+ *File:main.h
+ *Desc:header file with function prototypes
+ */
+char _putchar(char c);
+void _puts_recursion(char *s);
+
+#endif /* MAIN_H */
