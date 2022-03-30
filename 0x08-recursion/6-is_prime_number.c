06cef182af817e23a7c29452f7c33a6c37c33684
nair0lf32 <florianedem@gmail.com>
2022-03-30 13:18:29 +0100

    fix task 6

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -6,21 +6,28 @@
 */
 int is_prime_number(int n)
 {
+int i = n / 2;
 if (n <= 1)
 {
 return (0);
 }
-else if (n == 2)
+return (is_prime(n, i));
+}
+/**
+* is_prime - actually checks if n is prime
+* @n: number to be tested
+* @i: initial number
+* Return: 1 if n is prime, 0 if not
+*/
+int is_prime(int n, int i)
+{
+if (i <= 1)
 {
 return (1);
 }
-else if (n % 2 == 0)
+else if (n % i == 0)
 {
 return (0);
 }
-else
-{
-return (is_prime_number(n - 1));
+return (is_prime(n, i - 1));
 }
-}
-
