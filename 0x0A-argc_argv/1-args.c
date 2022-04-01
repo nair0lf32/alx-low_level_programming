9b4b3b33c9213ca5b820decced929ad55fd0fec5
nair0lf32 <florianedem@gmail.com>
2022-04-01 18:18:11 +0100

    fix task 1

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -5,8 +5,8 @@
 *@argv: array of arguments
 *Return: 0
 */
-int main(int argc, char *argv[])
+int main(int argc, char **argv __attribute__((unused)))
 {
-printf("%s\n", argc - 1);
+printf("%d\n", argc - 1);
 return (0);
 }
