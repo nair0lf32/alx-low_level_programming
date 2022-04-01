90cc6910814e937bc95a1698a5ec5327ecdaf099
nair0lf32 <florianedem@gmail.com>
2022-04-01 16:08:54 +0100

    another fix for task 0

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,9 +1,9 @@
 #ifndef MAIN_H
 #define MAIN_H
 /**
- *File:main.h
- *Desc:header file with function prototypes
- */
+*File:main.h
+*Desc:header file with function prototypes
+*/
 int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
@@ -24,5 +24,4 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
-
 #endif /* MAIN_H */
