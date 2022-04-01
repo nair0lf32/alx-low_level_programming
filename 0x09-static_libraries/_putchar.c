6c72c1e46999413ab90879576a4ba4272cadc31e
nair0lf32 <florianedem@gmail.com>
2022-04-01 17:29:33 +0100

    fixing task 0

--- 0x09-static_libraries/_putchar.c
+++ 0x09-static_libraries/_putchar.c
@@ -1,8 +1,8 @@
 #include <unistd.h>
 /**
-* _putchar - writes the character c to stdout
-* @c: The character to print
-* Return: 1 if success, -1 if error
+*_putchar - writes the character c to stdout
+*@c: The character to print
+*Return: 1 if success, -1 if error
 */
 int _putchar(char c)
 {
