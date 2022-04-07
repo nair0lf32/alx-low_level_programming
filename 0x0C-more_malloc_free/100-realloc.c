30f8ebf0c702243a8fd77590a9aed7a7fa6d8348
nair0lf32 <florianedem@gmail.com>
2022-04-07 22:42:14 +0100

    fix task 4

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -9,8 +9,16 @@
 */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-char *new_ptr;
+char *copy, *new_ptr;
 unsigned int i;
+if (ptr != NULL)
+{
+copy = ptr;
+}
+else
+{ 
+return (malloc(new_size)); 
+}
 if (new_size == old_size)
 {
 return (ptr);
@@ -18,49 +26,17 @@ return (ptr);
 if (new_size == 0 && ptr != NULL)
 {
 free(ptr);
-return (NULL);
-}
-if (ptr == NULL)
-{
-return (_calloc(new_size, 1));
+return (0); 
 }
 new_ptr = malloc(new_size);
 if (new_ptr == NULL)
 {
-return (NULL);
+return (0);
 }
-for (i = 0; i < old_size; i++)
+for (i = 0; i < (old_size || i < new_size); i++)
 {
-new_ptr[i] = ((char *)ptr)[i];
+*(new_ptr + i) = copy[i];
 }
 free(ptr);
 return (new_ptr);
-}
-/**
-*_calloc - allocates memory for an array using malloc
-*@nmemb: number of elements
-*@size: size of each element
-*Return: pointer to allocated memory or NULL if failed
-*/
-void *_calloc(unsigned int nmemb, unsigned int size)
-{
-char *ptr;
-unsigned int i;
-if (nmemb == 0 || size == 0)
-{
-return (NULL);
-}
-ptr = malloc(nmemb * size);
-if (ptr != NULL)
-{
-for (i = 0; i < (nmemb * size); i++)
-{
-ptr[i] = 0;
-}
-return (ptr);
-}
-else
-{
-return (NULL);
-}
 }
