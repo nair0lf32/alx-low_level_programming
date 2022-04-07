09896caf928eb7f137832e336c26465a1bbb4d91
nair0lf32 <florianedem@gmail.com>
2022-04-07 16:33:50 +0100

    fix task 2

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -8,15 +8,23 @@
 */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-void *ptr;
+char *ptr;
+unsigned int i;
 if (nmemb == 0 || size == 0)
 {
 return (NULL);
 }
 ptr = malloc(nmemb * size);
-if (ptr == NULL)
+if (ptr != NULL)
+{
+for (i = 0; i < (nmemb * size); i++)
+{
+ptr[i] = 0;
+}
+return (ptr);
+}
+else
 {
 return (NULL);
 }
-return (ptr);
 }
