e5cbd061141996666c9a09de5a290bef83e37982
nair0lf32 <florianedem@gmail.com>
2022-04-07 17:30:41 +0100

    fix task 3

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -16,12 +16,12 @@ if (min > max)
 return (NULL);
 }
 size = max - min + 1;
-ptr = malloc(size * sizeof(int));
+ptr = malloc(size *sizeof(int));
 if (ptr == NULL)
 {
 return (NULL);
 }
- for (i = 0; i < size; i++)
+for (i = 0; i < size; i++)
 {
 ptr[i] = min + i;
 }
