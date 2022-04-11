5ad9a7ae9858ee87ba8901c6d217281c5245072b
nair0lf32 <florianedem@gmail.com>
2022-04-11 17:29:47 +0100

    fix task 4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -6,18 +6,49 @@
 *@name: pointer to dog's name
 *@age: dog's age
 *@owner: pointer to dog's owner
-*Return: a new dof, or NULL if failed
+*Return: a new dog, or NULL if failed
 */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-dog_t *new_dog;
-new_dog = malloc(sizeof(dog_t));
-if (new_dog == NULL)
+int i = 0, j = 0, k;
+dog_t *doge;
+while (name[i] != '\0')
 {
+i++;
+}
+while (owner[j] != '\0')
+{
+j++;
+}
+doge = malloc(sizeof(dog_t));
+if (doge == NULL)
+{
+free(doge);
 return (NULL);
 }
-new_dog->name = name;
-new_dog->age = age;
-new_dog->owner = owner;
-return (new_dog);
+doge->name = malloc(i *sizeof(doge->name));
+if (doge->name == NULL)
+{
+free(doge->name);
+free(doge);
+return (NULL);
+}
+for (k = 0; k <= i; k++)
+{
+doge->name[k] = name[k];
+}
+doge->age = age;
+doge->owner = malloc(j *sizeof(doge->owner));
+if (doge->owner == NULL)
+{
+free(doge->owner);
+free(doge->name);
+free(doge);
+return (NULL);
+}
+for (k = 0; k <= j; k++)
+{
+doge->owner[k] = owner[k];
+}
+return (doge);
 }
