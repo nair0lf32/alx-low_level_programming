4f740bb0d636be3794b9053c085d734bcd10be9f
nair0lf32 <florianedem@gmail.com>
2022-04-11 16:43:29 +0100

    fix task 2

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -12,26 +12,26 @@ return;
 }
 if (d->name == NULL)
 {
-printf("Name: nil");
+printf("Name: nil\n");
 }
 else
 {
-printf("Name: %s", d->name);
+printf("Name: %s\n", d->name);
 }
-if (d->age < 0)
+if (d->age == 0)
 {
-printf("Age: nil");
+printf("Age: nil\n");
 }
 else
 {
-printf("Age: %f", d->age);
+printf("Age: %f\n", d->age);
 }
 if (d->owner == NULL)
 {
-printf("Owner: nil");
+printf("Owner: nil\n");
 }
 else
 {
-printf("Owner: %s", d->owner);
+printf("Owner: %s\n", d->owner);
 }
 }
