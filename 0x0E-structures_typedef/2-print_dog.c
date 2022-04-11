d00e506607e94a08e6542fe3633977acb6ac174c
nair0lf32 <florianedem@gmail.com>
2022-04-11 16:48:52 +0100

    fix task 2

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -17,14 +17,7 @@ else
 {
 printf("Name: (nil)\n");
 }
-if (d->age > 0)
-{
 printf("Age: %f\n", d->age);
-}
-else
-{
-printf("Age: (nil)\n");
-}
 if (d->owner != NULL)
 {
 printf("Owner: %s\n", d->owner);
