80c2e9c1b22ca61f817761c5ff1c8e10957fac46
nair0lf32 <florianedem@gmail.com>
2022-04-11 13:44:24 +0100

    fix task 0

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -1,8 +1,4 @@
 #ifndef OBJECT_LIKE_MACRO_H
 #define OBJECT_LIKE_MACRO_H
-/**
-*File:object_like_macro.h
-*Desc:header to define size macro
-*/
 #define SIZE 1024
-#endif /* object_like_macro.h */
+#endif
