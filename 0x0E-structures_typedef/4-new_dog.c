be3252084c112ed91150ceca9f816ea0d9a66102
nair0lf32 <florianedem@gmail.com>
2022-04-11 17:20:28 +0100

    do task 4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -10,45 +10,14 @@
 */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-int i = 0, j = 0, k;
-dog_t *doge;
-while (name[i] != '\0')
+dog_t *new_dog;
+new_dog = malloc(sizeof(dog_t));
+if (new_dog == NULL)
 {
-i++;
-}
-while (owner[j] != '\0')
-{
-j++;
-}
-doge = malloc(sizeof(dog_t));
-if (doge == NULL)
-{
-free(doge);
 return (NULL);
 }
-doge->name = malloc(i * sizeof(doge->name));
-if (doge->name == NULL)
-{
-free(doge->name);
-free(doge);
-return (NULL);
-}
-for (k = 0; k <= i; k++)
-{
-doge->name[k] = name[k];
-}
-doge->age = age;
-doge->owner = malloc(j * sizeof(doge->owner));
-if (doge->owner == NULL)
-{
-free(doge->owner);
-free(doge->name);
-free(doge);
-return (NULL);
-}
-for (k = 0; k <= j; k++)
-{
-doge->owner[k] = owner[k];
-}
-return (doge);
+new_dog->name = name;
+new_dog->age = age;
+new_dog->owner = owner;
+return (new_dog);
 }
