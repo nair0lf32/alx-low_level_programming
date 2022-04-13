093469e1fa25abd0bf118bb27d11889d60301dd5
nair0lf32 <florianedem@gmail.com>
2022-04-13 23:04:54 +0100

    fix task 0

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,6 +1,4 @@
 #include "function_pointers.h"
-#include <stdio.h>
-#include <stdlib.h>
 /**
 *print_name - does what it says
 *@name: name to print (string)
@@ -9,7 +7,7 @@
 */
 void print_name(char *name, void (*f)(char *))
 {
-if (name == NULL || f == NULL)
+if (f == NULL)
 {
 return;
 }
--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,5 +1,9 @@
 #ifndef FUNTION_POINTERS_H
 #define FUNCTION_POINTERS_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
 /**
  *File:function_pointers.h
  *Desc:header file with function prototypes
