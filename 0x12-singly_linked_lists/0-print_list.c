618711d4c2ffc05655f91d6570cf5e83ae65d22b
nair0lf32 <florianedem@gmail.com>
2022-04-22 19:25:35 +0100

    fix task 0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -6,14 +6,15 @@
 */
 size_t print_list(const list_t *h)
 {
-size_t count = 0;
-const list_t *tmp;
-tmp = h;
-while (tmp)
+unsigned int len = 0;
+while (h)
 {
-count++;
-printf("%s\n", tmp->str);
-tmp = tmp->next;
+if (!h->str)
+	printf("[0] (nil)\n");
+else
+	printf("[%d] %s\n", h->len, h->str);
+h = h->next;
+len++;
 }
-return (count);
+return (len);
 }
