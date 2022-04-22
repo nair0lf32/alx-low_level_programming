cdd7ad8b5a308b571b80fb273c4f6c96dffb5961
nair0lf32 <florianedem@gmail.com>
2022-04-23 00:14:24 +0100

    fix task 2

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -8,22 +8,15 @@ list_t *add_node(list_t **head, const char *str)
 {
 list_t *new_node;
 new_node = malloc(sizeof(list_t));
-if (new_node != NULL)
+if (!new_node)
 {
+return (new_node);
+}
 new_node->str = strdup(str);
 new_node->len = _strlen(str);
 new_node->next = *head;
-}
-else
-{
-return (NULL);
-}
-if (*head != NULL)
-{
-new_node->next = *head;
-}
 *head = new_node;
-return (new_node);
+return (*head);
 }
 /**
 *_strlen - returns the length of a string
