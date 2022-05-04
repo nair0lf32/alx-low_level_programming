bc77ac924fa9b60ea11e73d02bd57cff209a8e8b
nair0lf32 <florianedem@gmail.com>
2022-05-04 22:03:29 +0100

    fix task 0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
-*read_textfile - Reads a text file and prints it to stdout.
-*@filename: pointer to the name of the file.
-*@letters: number of letters the function should read and print.
-*Return: 0 or O/w - the actual number of bytes the function can read and print.
+*read_textfile - Reads a text file and prints it to stdout
+*@filename: pointer to the name of the file
+*@letters: number of letters the function should read and print
+*Return: 0 or actual number of bytes the function can read and print
 */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
