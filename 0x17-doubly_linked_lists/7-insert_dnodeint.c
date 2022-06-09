422f6a15bfcd32cbd08a7ad3f51be798128151c4
nair0lf32 <florianedem@gmail.com>
2022-06-09 21:55:40 +0100

    fix task 7

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -8,45 +8,32 @@
 */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-dlistint_t *new_node;
-dlistint_t *temp;
-unsigned int i;
-if (h == NULL)
-{
-return (NULL);
-}
-new_node = malloc(sizeof(dlistint_t));
-if (new_node == NULL)
-{
-return (NULL);
-}
-new_node->n = n;
+dlistint_t *temp = *h, *new;
 if (idx == 0)
 {
-new_node->next = *h;
-new_node->prev = NULL;
-if (*h)
-{
-(*h)->prev = new_node;
+return (add_dnodeint(h, n));
 }
-*h = new_node;
-return (new_node);
-}
-temp = *h;
-for (i = 0; i < idx - 1; i++)
+for (; idx != 1; idx--)
 {
-if (temp->next == NULL)
+temp = temp->next;
+if (temp == NULL)
 {
 return (NULL);
 }
-temp = temp->next;
 }
-new_node->next = temp->next;
-new_node->prev = temp;
-if (temp->next)
+if (temp->next == NULL)
 {
-temp->next->prev = new_node;
+return (add_dnodeint_end(h, n));
 }
-temp->next = new_node;
-return (new_node);
+new = malloc(sizeof(dlistint_t));
+if (new == NULL)
+{
+return (NULL);
+}
+new->n = n;
+new->prev = temp;
+new->next = temp->next;
+temp->next->prev = new;
+temp->next = new;
+return (new);
 }
