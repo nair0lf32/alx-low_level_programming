3fc163f9d4241dc31d2cce385abe1e05883b76c2
nair0lf32 <florianedem@gmail.com>
2022-06-14 22:10:27 +0100

    do task 2

---
+++ 0x18-dynamic_libraries/00-operations.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+/**
+*add - add two numbers
+*@a: first number
+*@b: second number
+*Return: the sum
+*/
+int add(int a, int b){
+return a + b;
+}
+/**
+*sub - substrat a number
+*@a: first number
+*@b: second number
+*Return: the diff
+*/
+int sub(int a, int b){
+return a - b;
+}
+/**
+*mul - multiply numbers
+*@a: first number
+*@b: second number
+*Return: the product
+*/
+int mul(int a, int b){
+return a * b;
+}
+/**
+*div - divides two numbers
+*@a: first number
+*@b: second number
+*Return: the quotient
+*/
+int div(int a, int b){
+return a / b;
+}
+/**
+*mod - modulo
+*@a: first number
+*@b: second number
+*Return: the rest of division
+*/
+int mod(int a, int b){
+return a % b;
+}
