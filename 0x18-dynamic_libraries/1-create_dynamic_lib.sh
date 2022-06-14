68730913f5ef7ca7051234c0a3951237851430f9
nair0lf32 <florianedem@gmail.com>
2022-06-14 17:54:49 +0100

    fix task 1

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-gcc -g -fPIC -Wall -Werror -Wextra -pedantic *.c -shared -o liball.so
+gcc -g -fPIC -Werror -Wextra -pedantic *.c -shared -o liball.so
 export LD_LIBRARY_PATH=$PWD:$LD_LIBRARY_PATH
